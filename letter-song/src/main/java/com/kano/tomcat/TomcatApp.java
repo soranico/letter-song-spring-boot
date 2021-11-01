21acb0d2c9c34efcff3f0967417e920e54b98489
xinge <xinge@dtstack.com>
2021-11-01 10:28:04 +0800

    v2.5.3

---
+++ letter-song/src/main/java/com/kano/tomcat/TomcatApp.java
@@ -0,0 +1,12 @@
+package com.kano.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatApp.class,args);
+	}
+}
